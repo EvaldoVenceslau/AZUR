78e2f7503984d2d307ecb38b54043c1fa208daa7
evaldo.venceslau@hotmail.com <evaldo.venceslau@hotmail.com>
2024-11-14 20:13:00 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Empreendimentos from './components/Empreendimentos';
+import FloatingButtonModal from './components/FloatingButtonModal';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <Empreendimentos />
+            <Footer />
+            <FloatingButtonModal />
+        </div>
+    );
 }
 
 export default App;
